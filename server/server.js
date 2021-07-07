a3ea36a44f3c7eaa864e1df5672ae18d2f62d1c3
Kyle Friel <businessfriel@gmail.com>
2021-07-07 14:12:50 -0400

    Express server setup.

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+import express from "express";
+import cors from "cors";
+
+import todosRoutes from "./routes/todos.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("HELLO THERE");
+});
+
+app.use("/todos", todosRoutes);
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
