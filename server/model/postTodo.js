11e736104ac3cdfab7d4d694316d9e8d289a7221
Kyle Friel <businessfriel@gmail.com>
2021-07-07 15:26:18 -0400

    Schema edit - added isCompleted.

--- server/model/postTodo.js
+++ server/model/postTodo.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const todoSchema = mongoose.Schema({
     task: String,
     description: String,
+    isCompleted: {
+        type: Boolean,
+        default: false,
+    },
     dueDate: Date,
     createdAt: {
         type: Date,
