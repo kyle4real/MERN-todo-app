c26d85da4d060958d12ee3f48de42ed296a6b006
Kyle Friel <businessfriel@gmail.com>
2021-07-08 12:12:32 -0400

    Todo Form additional input fields, buttons, styles..

--- client/src/components/TodoForm/TodoForm.js
+++ client/src/components/TodoForm/TodoForm.js
@@ -1,15 +1,30 @@
 import React from "react";
 
-import { Container, TextField, Grid } from "@material-ui/core";
+import { Container, TextField, Grid, Button } from "@material-ui/core";
 import useStyle from "./style";
 
 const TodoForm = () => {
     const classes = useStyle();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted");
+    };
     return (
         <Container maxWidth="md">
-            <form className={`${classes.form} ${classes.root}`} autoComplete="off">
-                <Grid container alignItems="stretch" spacing={3} className={classes.formRow}>
-                    <Grid item xs={12} sm={12} md={6}>
+            <form
+                className={`${classes.form} ${classes.root}`}
+                autoComplete="off"
+                onSubmit={(e) => handleSubmit(e)}
+            >
+                <Grid
+                    container
+                    alignItems="stretch"
+                    justifyContent="center"
+                    spacing={3}
+                    className={classes.formRow}
+                >
+                    <Grid item xs={12} md={6}>
                         <TextField
                             name="task-input"
                             id="task-input"
@@ -19,7 +34,7 @@ const TodoForm = () => {
                             placeholder="make reservations, walk the dog, jog at sunset"
                         />
                     </Grid>
-                    <Grid item xs={12} sm={12} md={6}>
+                    <Grid item xs={12} md={6}>
                         <TextField
                             id="outlined-textarea"
                             label="Task Description"
@@ -27,6 +42,40 @@ const TodoForm = () => {
                             multiline
                             variant="outlined"
                         />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <TextField
+                            id="datetime-local"
+                            label="Due Date"
+                            type="datetime-local"
+                            defaultValue="2017-05-24T10:30"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={4} className={classes.buttonGridItem}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            type="submit"
+                            fullWidth
+                        >
+                            Add Task
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            size="large"
+                            type="submit"
+                            fullWidth
+                        >
+                            Reset
+                        </Button>
                     </Grid>
                 </Grid>
             </form>
--- client/src/components/TodoForm/style.js
+++ client/src/components/TodoForm/style.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 export default makeStyles((theme) => ({
     root: {
         "& .MuiTextField-root": {
-            margin: theme.spacing(1),
+            // margin: theme.spacing(1),
             width: "100%",
         },
     },
